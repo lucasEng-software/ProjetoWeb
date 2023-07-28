4f51fdb6a99333cc5f16f65f340b8e2aae386ac0
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-07-28 14:34:55 -0300

    criação da exportação de controller e usecase

---
+++ src/useCases/CreateUser/Index.ts
@@ -0,0 +1,17 @@
+import { UsersRespository } from '../../repositories/Implementations/UsersRepository';
+import { MailtrapMailProvider } from './../../providers/Implementations/MailtrapMailProvider';
+import { CreateUserController } from './CreateUserController';
+import { CreateUserUseCase } from './CreateUserUseCase';
+const mailtrapMailProvider = new MailtrapMailProvider();
+const userRespository = new UsersRespository();
+
+const createUserUseCase = new CreateUserUseCase(
+    userRespository,
+    mailtrapMailProvider,
+)
+
+const createUserControler = new CreateUserController(
+    createUserUseCase,  
+)
+
+export { createUserUseCase, createUserControler}
