73be0fd8739a9a5ea4ed03d518e0e4b6a11bad8f
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-07-28 14:34:00 -0300

    Criação do controller de criação de Usuário

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import {CreateUserUseCase } from "./CreateUserUseCase"
+export class CreateUserController{
+
+    constructor(
+        private createUserUseCase: CreateUserUseCase,
+    ){}
+
+    async handle( request: Request, response: Response): Promise<Response> {
+        const { nome, email, password } = request.body;
+
+        try{
+            await this.createUserUseCase.execute({
+            nome,
+            email,
+            password 
+            })
+            return response.status(201).send();
+        } catch (err) {
+            return response.status(400).json({
+                message:err.messagr || 'Erro inesperado'
+            })
+        }
+    }
+}
