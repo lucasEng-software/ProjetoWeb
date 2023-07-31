5e2ecb892a6fef2a9cfc84bfa072a3c5b1bb7e0a
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-07-31 13:39:52 -0300

    inclusão do método de listagem de usuarios na IU

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -2,6 +2,6 @@ import { Usuario } from "../entities/Usuario";
 
 export interface IUsersRepository {
     findByEmail(email:string): Promise<Usuario>;
-    //createUser({name, email}: Omit<Usuario,"id"> ):Promise<void>
     save(usuario:Usuario): Promise<void>;
+    list():Promise<Usuario[]>;
 }
