fcaf2218d73fe19a62ef06f7bef443c2f1035e02
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-07-31 13:39:28 -0300

    criação da rota de get de usuarios

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express"
 import { createUserControler } from "./useCases/CreateUser/Index";
+import { getUsersControler } from "./useCases/GetUsers/Index";
 
 
 const router = Router()
 
 router.post('/usuario', (request, response) =>{
     return createUserControler.handle(request, response)
+});
+
+
+router.get('/usuario', (request, response) =>{
+    return getUsersControler.handle(request, response) // Funcionando!! Implementar agora o retorno JSON, e posteriormente o banco de dados e tratamento de excessões
 });
 
 export{ router }
