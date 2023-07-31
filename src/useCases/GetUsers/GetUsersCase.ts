df1f101d22acb8cd33334a6bf65b13cd3dbda0c5
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-07-31 13:41:27 -0300

    implementação da classe de consulta de usuários

---
+++ src/useCases/GetUsers/GetUsersCase.ts
@@ -0,0 +1,20 @@
+import { Usuario } from '../../entities/Usuario';
+import { IMailProvider } from '../../providers/IMailProvider';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+export class GetUsersCase {
+
+    constructor (
+        private usersRespository: IUsersRepository,
+    ){}
+    
+    async execute(): Promise<Usuario[]> {
+        const listaDeUsuarios = await this.usersRespository.list();
+
+        if (!listaDeUsuarios){
+            throw new Error ('Não existem usuários cadastrados');
+        }
+
+        return listaDeUsuarios;
+    }
+}
