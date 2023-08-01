0be78c1a8527d178d448a4f3e923cdb38a7b4b9e
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-08-01 14:33:46 -0300

    alteração da assinatura do metodo save

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -1,7 +1,8 @@
 import { Usuario } from "../entities/Usuario";
 
+
 export interface IUsersRepository {
     findByEmail(email:string): Promise<Usuario>;
-    save(usuario:Usuario): Promise<Usuario[]>;
+    save(usuario:Usuario): Promise<{ status: string; mensagem: string }>;
     list():Promise<Usuario[]>;
 }
