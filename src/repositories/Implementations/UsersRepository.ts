5d8258fc648f031f3f4599de256f15c5633faab0
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-08-01 14:35:00 -0300

    implementação do banco para criação e busca do usuario

--- src/repositories/Implementations/UsersRepository.ts
+++ src/repositories/Implementations/UsersRepository.ts
@@ -1,18 +1,34 @@
 import { Usuario } from '../../entities/Usuario';
+import { Request, Response } from "express";
 import { IUsersRepository } from './../IUsersRepository';
+const bd = require('../Implementations/ConexaoBD')
 export class UsersRespository implements IUsersRepository{
     private usuarios: Usuario[] = [];
 
     async findByEmail(email: string): Promise<Usuario> {
-        const usuario = this.usuarios.find(usuario => usuario.email === email);
-
+        const usuario = bd.findOne({
+            where: {
+              email: email
+            }
+          });
         return usuario;
-
     }
 
-    async save (usuario: Usuario): Promise<Usuario[]>{
-        this.usuarios.push(usuario);
-        return this.usuarios;
+    async save (usuario: Usuario): Promise<{ status: string; mensagem: string }>{
+        try{
+            await bd.create(usuario);
+            const retornoSucesso = {
+                status:'sucesso',
+                mensagem: 'Usuário cadastrado com sucesso!',
+            };
+            return retornoSucesso;
+        } catch (error) {
+            const retornoError = {
+                status:'error',
+                mensagem: 'Usuário não cadastrado com sucesso!',
+            };
+            return retornoError;
+        }    
     }
 
     async list(): Promise<Usuario[]>{
